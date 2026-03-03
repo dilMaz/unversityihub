9309ae3a3cf9cf5abf5cfc36772841f10a969fce
dilMaz <atapattudilan@gmail.com>
2026-03-03 16:51:36 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+//password -   iPTXvEiynbCydYHa
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use("/",(req, res, next) => {
+    res.send("Hello World!");
+});
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://admin:iPTXvEiynbCydYHa@cluster0.bhaclhu.mongodb.net/")
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err) => console.log((err)));
